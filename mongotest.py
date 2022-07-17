6bbab0963c1317147ad20996c83b348ec8a15ead
Vishal kumar <vishalkumarrss2022@gmail.com>
2022-07-17 19:27:54 +0530

    this is my first code push in git sec time

--- mongotest.py
+++ mongotest.py
@@ -10,6 +10,16 @@ d = {
     "email" : "vishal@gmail.com",
     "surname" : "kumar"
 }
+d = {
+    "name":"vishal",
+    "email" : "vishal@gmail.com",
+    "surname" : "kumar"
+}
+d = {
+    "name":"vishal",
+    "email" : "vishal@gmail.com",
+    "surname" : "kumar"
+}
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d )
